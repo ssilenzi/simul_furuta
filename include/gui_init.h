72867219818e0550865e03be39f0eede4bdf76a2
Simone Silenzi <s.silenzi1@gmail.com>
2019-04-08 10:40:09 +0200

    Refusi

--- include/gui_init.h
+++ include/gui_init.h
@@ -12,15 +12,6 @@
 #include "types.h"
 #include "os.h"
 
-//int dist;
-int colscr, colbck;
-int pos0xasso, pos0yasso, pos0xlato, pos0ylato, pos0xalto, pos0yalto;
-TwoPoints resetasso;
-//int resetasso1x, resetasso1y, resetasso2x, resetasso2y;
-int resetlato1x, resetlato1y, resetlato2x, resetlato2y;
-int resetalto1x, resetalto1y, resetalto2x, resetalto2y;
-int colmdl, colmdl2, colrif;
-
 //Static GUI
 int gui_init();
 
