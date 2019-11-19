376b8e16e5083c5890bb24cda48f2fd34be7dc24
fpetracci <petracci.francesco@gmail.com>
2019-11-19 18:32:43 +0100

    Tutto pronto per pthread

--- include/types.h
+++ include/types.h
@@ -50,8 +50,10 @@ typedef struct {
 
 
 // types for handling pthread
-#include <stdbool.h>        // BRUTTO QUI, pero` comodissimo
-                            // Alternativamente potremmo mettere la definizione in ptask.h
+#include <stdbool.h>
+#include <pthread.h>
+    // BRUTTO QUI, pero` comodissimo
+    // Alternativamente potremmo mettere la definizione in ptask.h
 
 typedef struct task_par {
 	bool 		in_use;		// structure is free or in use
